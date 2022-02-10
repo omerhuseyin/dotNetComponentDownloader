fc17e60cc7d6f352f53f83b1e506123afcae8c0d
Ömer Hüseyin GÜL <omerhuseyingul@outlook.com>
2022-02-10 16:39:06 +0300

    Code Editing and Improvements

--- dotnetUIFrameworkDownloader/MainForm.cs
+++ dotnetUIFrameworkDownloader/MainForm.cs
@@ -16,35 +16,21 @@
             InitializeComponent();
         }
 
-        private Point lastPoint = default(Point);
-        private Uri globalUri = default(Uri);
-        private string downloadPath = default(string);
-        private string downloadSize = default(string);
-        private string targetFramework = default(string);
-        private string combinedFile = default(string);
+        #region Global Variables
+        private Point lastPoint = default;
+        private Uri globalUri = default;
+        private string downloadPath = default;
+        private string downloadSize = default;
+        private string targetFramework = default;
+        private string combinedFile = default;
+        #endregion
 
-
-
-        private void pathButton_Click(object sender, EventArgs e)
-        {
-            PlaySound("OPEN_SOUND");
-            pathButton.Text = "Selecting...";
-            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
-            folderDlg.ShowNewFolderButton = true;
-            DialogResult result = folderDlg.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                downloadPath = folderDlg.SelectedPath;
-                pathButton.Text = downloadPath;
-            }
-
-            else if (result == DialogResult.Cancel)
-            {
-                pathButton.Text = "Select Location";
-            }
-        }
-
-
+        #region File Download Functions
+        /// <summary>
+        /// File Download Controller Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
             try
@@ -61,6 +47,11 @@
             catch (Exception ex) { MessageBox.Show(ex.Message);}
         }
 
+        /// <summary>
+        /// Returns the MB value of the file to be downloaded
+        /// </summary>
+        /// <param name="uriPath"></param>
+        /// <returns></returns>
         private static string GetFileSize(Uri uriPath)
         {
             try
@@ -75,14 +66,14 @@
                     return fileSizeInMegaByte + " MB";
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return null;
-            }
-            
+            catch (Exception ex) { MessageBox.Show(ex.Message); return null; }         
         }
 
+        /// <summary>
+        /// The Event that Starts the Installation Process.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void installButton_Click(object sender, EventArgs e)
         {
             try
@@ -113,30 +104,18 @@
 
         }
 
-        private void PlaySound(string param)
-        {
-            try
-            {
-                using (SoundPlayer player = new SoundPlayer())
-                {
-                    if (param == "SUCCESS_SOUND") player.SoundLocation = @"success.wav";
-                    else if (param == "OPEN_SOUND") player.SoundLocation = @"open.wav";
-                    else if (param == "WARNING_SOUND") player.SoundLocation = @"warning.wav";
-                    else if (param == "ERROR_SOUND") player.SoundLocation = @"error.wav";
-                    player.Play();
-                    player.Dispose();
-                }
-            }
-            catch (Exception ex) { MessageBox.Show(ex.Message);}
-        }
-            
+        /// <summary>
+        /// It is the Event that Triggers the Commands to be Executed Once the File Download is Complete.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Completed(object sender, AsyncCompletedEventArgs e)
         {
-            downloadPath = string.Empty;
-            downloadSize = string.Empty;
-            targetFramework = string.Empty;
-            combinedFile = string.Empty;
-            globalUri = null;
+            downloadPath = default;
+            downloadSize = default;
+            targetFramework = default;
+            combinedFile = default;
+            globalUri = default;
             installButton.Text = "Install";
             installButton.Enabled = true;
             frameworkList.Text = "";
@@ -145,7 +124,62 @@
             PlaySound("SUCCESS_SOUND");
             lblDownloadStatus.Text = ($"{targetFramework} Successfully Installed!");
         }
+        #endregion
 
+        #region Other Functions
+        /// <summary>
+        /// Download Location Selecting Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pathButton_Click(object sender, EventArgs e)
+        {
+            PlaySound("OPEN_SOUND");
+            pathButton.Text = "Selecting...";
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                downloadPath = folderDlg.SelectedPath;
+                pathButton.Text = downloadPath;
+            }
+
+            else if (result == DialogResult.Cancel)
+            {
+                pathButton.Text = "Select Location";
+            }
+        }
+
+        /// <summary>
+        /// It is the function that plays sound effects after the running events.
+        /// </summary>
+        /// <param name="param"></param>
+        private void PlaySound(string param)
+        {
+            try
+            {
+                using (SoundPlayer player = new SoundPlayer())
+                {
+
+                    player.SoundLocation = 
+                        param == "SUCCESS_SOUND" ? @"success.wav" : 
+                        param == "OPEN_SOUND" ? @"open.wav" : 
+                        param == "WARNING_SOUND" ? @"warning.wav" : 
+                        param == "ERROR_SOUND" ? @"error.wav" : null;
+                    
+                    player.Play();
+                    player.Dispose();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message);}
+        }
+
+        /// <summary>
+        /// It is the event that controls whether the download can be started.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Controller_Tick(object sender, EventArgs e)
         {
             if (downloadPath == string.Empty && targetFramework == string.Empty)
@@ -153,7 +187,6 @@
 
             else if (downloadPath != string.Empty && targetFramework != string.Empty)
                 installButton.Enabled =true;
-
         }
 
         private void frameworkList_SelectedValueChanged(object sender, EventArgs e)
@@ -200,5 +233,6 @@
         {
             System.Diagnostics.Process.Start("https://github.com/omerhuseyingul/dotnet-ui-components");
         }
+        #endregion
     }
 }
